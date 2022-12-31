6f1268ab49de835c5f0cce5677518597087664fb
Wiliam Roza <devfrontwill@gmail.com>
2022-12-30 22:00:48 -0300

    Projeto concluido

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,8 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
-
-    console.log(validaNumerosRepetidos(cpf));
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        campo.setCustomValidity('Esse cpf não é válido')
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -20,4 +20,40 @@ function validaNumerosRepetidos(cpf) {
     ]
 
     return numerosRepetidos.includes(cpf)
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
